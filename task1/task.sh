165221e770032ba3e3359d7e0781a7c115113e5e
MaryEs23 <54851087+MaryEs23@users.noreply.github.com>
2019-09-27 16:09:15 +0300

    Update task.sh

--- task1/task.sh
+++ task1/task.sh
@@ -10,7 +10,7 @@ do
                 then
                         if [[ "$file" = *.mp4 || "$file" = *.mov || "$file" = *.avi || "$file" = *.mp3 ]]
                         then
-                                a=$(ffprobe -i $file -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal);
+                                a=$(ffprobe -i "$file" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal);
                                 echo $a>>$path/files/duration.txt
                         else
                                 a="None";
