c22ac0336fac343f29843e42b6e475cf5fdbf369
MaryEs23 <54851087+MaryEs23@users.noreply.github.com>
2019-09-30 18:56:14 +0300

    Delete task2.sh

--- task2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/andrejv/maxima 
-sudo apt-get install g++-mingw-w64-i686 cmake nsis wine automake texinfo rsync p7zip-full tcl-dev tk-dev texlive g++ libgl1-mesa-dev gettext
-cd crosscompile-windows/build
-cmake ..
-make
-make package
-mv maxima /tmp
