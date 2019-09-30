91dbc9831444a1810427717efb647b3537a26d55
MaryEs23 <54851087+MaryEs23@users.noreply.github.com>
2019-09-30 22:58:26 +0300

    Update task2.sh

--- task2/task2.sh
+++ task2/task2.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-git clone https://github.com/andrejv/maxima 
+#!/bin/bash 
 sudo apt-get install g++-mingw-w64-i686 cmake nsis wine automake texinfo rsync p7zip-full tcl-dev tk-dev texlive g++ libgl1-mesa-dev gettext
+git clone https://github.com/andrejv/maxima 
 cd maxima/crosscompile-windows/build
 cmake ..
 make
